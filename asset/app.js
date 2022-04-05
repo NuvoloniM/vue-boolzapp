20f54363a38e7dae5b58036906c22798e28f0672
NuvoloniM <nuvoloni.m@gmail.com>
2022-04-05 16:11:46 +0200

    funzione per filtrare

--- asset/app.js
+++ asset/app.js
@@ -1,3 +1,26 @@
+/*
+Milestone 1
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Milestone 2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+● Click sul contatto mostra la conversazione del contatto cliccato
+Milestone 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+“enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+un “ok” come risposta, che apparirà dopo 1 secondo.
+Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
+Consigli utili:
+● Si possono trascurare le scrollbar verticali, sia nel pannello dei messaggi, che nella lista dei contatti
+● I pulsanti e le icone possono non funzionare (a parte l’invio del messaggio)
+● Per gestire le date, può essere utile la libreria day.js
+● La struttura dell’array dei contatti potrebbe avere questa forma:
+*/
 var app = new Vue (
     {
         el: '#root',
@@ -6,6 +29,7 @@ var app = new Vue (
             currentIndex:'0',
             //creo variabile che, in maniera dinamica, cambia a seconda dell'input.value a cui ho messo il v-modal
             newMsg:'',
+            searchWord:'',
             profilo: {
                 name: 'Sofia',
                 avatar: '_io'
@@ -175,6 +199,18 @@ var app = new Vue (
             ]
             
         },
+        //sezione computed metto funzioni un po più complesse
+        computed:{
+            //creo funzione per filtrare l'array dei contatti
+            filteredList(){
+                //restituisci la lista dei contatti nei data ma filtrata da una funzione
+                return this.contacts.filter(contact => {
+                    /*la funzione che filtra è : prendi ogni nome dell'array contatti, trasfomra tutto in lower case, 
+                    controlla se la lettera o parola messa in input sia contenuta nell'array nome*/
+                    return contact.name.toLowerCase().includes(this.searchWord.toLowerCase())
+                })
+            }
+        },
         methods: {
             //funzione per selezionare l'id dell'oggetto contacts per poter selezionare i suoi dati, quali array messaggi nome e img 
             takeIndex: function(index){
@@ -212,7 +248,7 @@ var app = new Vue (
                 let x = test.split(' ');
                 //per come è fatto il nostro array Date, mi serve il secondo elemento splitato, quello delle ore
                 return x[1];
-            }
+            },
         }
     }
 )
